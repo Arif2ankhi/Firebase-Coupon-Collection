253ede956aaa60ea68aeafc17c6fa009d33f6929
Arif2ankhi <arif2ankhi@gmail.com>
2024-11-18 01:43:55 +0000

    Added Fast Marquee in brand logo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import { useLoaderData } from 'react-router-dom';
 import ServiceCard from '../ServiceCard/ServiceCard';
+import Marquee from 'react-fast-marquee';
 
 const Home = () => {
 
@@ -14,17 +15,28 @@ console.log(services);
         <div className='w-11/12'>
           <Banner></Banner>
           <section>
-          <div className="text-3xl font-extrabold justify-center text-center text-red-800">
+          <div className="text-3xl font-extrabold justify-center text-center text-red-800 m-12">
             Top Brands
           </div>
-          <div className='flex gap-10 w-[200px] h-[160px]'>
+
+          <Marquee pauseOnHover={true} speed={100} className=''>
+          <div className='flex gap-8 md:gap-32 w-[200px] h-[160px]'>
             <img src="../../../public/Images/daraz.png" alt="" />
             <img src="../../../public/Images/evally.jpg" alt="" />
             <img src="../../../public/Images/foodpanda.jpg" alt="" />
             <img src="../../../public/Images/chaldal.jpeg" alt="" />
             <img src="../../../public/Images/rokomari.jpeg" alt="" />
+            <img src="../../../public/Images/Bikroy.png" alt="" />
+            <img src="../../../public/Images/picaaboo1.jpg" alt="" />
           </div>
-          </section>
+          </Marquee>
+        </section>
+        
+          <section>
+            <div className="text-3xl font-extrabold justify-center text-center text-red-800 m-12">
+                Brands On Sell
+            </div>
+        </section>
           <div>
             {
                 services.map(service =><ServiceCard  service={service}></ServiceCard>)
